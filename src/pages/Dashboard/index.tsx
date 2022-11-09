bc96fca3a6c86fe3383f77fa5d730c2f69cc0d6a
Fabianno <fabiannootavio08@gmail.com>
2022-11-09 17:11:58 -0300

    feat: add order screen

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,15 +1,80 @@
-import { View, Text, Button } from 'react-native';
+import { View, Text, SafeAreaView, TouchableOpacity, TextInput, StyleSheet } from 'react-native';
 import { useAuth } from '../../contexts/AuthContext';
+import { useState } from 'react';
+import { useNavigation } from '@react-navigation/native';
+import { StackParamsList } from '../../routes/app.routes';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack'
 
 export default function Dashboard(){
+
     const { signOut } = useAuth();
+
+    const navigation = useNavigation<NativeStackNavigationProp<StackParamsList>>();
+    const [table, setTable] = useState('');
+
+    async function openOrder(){
+        if(table === ''){
+            return;
+        }
+
+        navigation.navigate('Order', {table: table, order_id: ''});
+    }
+
     return(
-        <View>
-            <Text>Tela Dashboard</Text>
-            <Button
-                title= 'Sair do app'
-                onPress={signOut}
+        <SafeAreaView style={styles.container}>
+            <Text style={styles.title}>Novo pedido</Text>
+            <TextInput 
+            placeholder='Número da mesa'
+            placeholderTextColor='#f0f0f0'
+            style={styles.input}
+            keyboardType='numeric'
+            value={table}
+            onChangeText={setTable}
             />
-        </View>
+
+            <TouchableOpacity style={styles.button} onPress={openOrder}>
+                <Text style={styles.buttonText}>Abrir mesa</Text>
+            </TouchableOpacity>
+        </SafeAreaView>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingVertical: 15,
+        backgroundColor: '#1d1d2e'
+    },
+    title: {
+        fontSize: 30,
+        fontWeight: 'bold',
+        color: '#FFF',
+        marginBottom: 24
+    },
+    input: {
+        width: '90%',
+        height: 60,
+        backgroundColor: '#101026',
+        borderRadius: 4,
+        paddingHorizontal: 8,
+        textAlign: 'center',
+        fontSize: 22,
+        color: '#FFF'
+    },
+    button: {
+        width: '90%',
+        height: 40,
+        backgroundColor: '#3fffa3',
+        borderRadius: 4,
+        marginVertical: 12,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    buttonText: {
+        fontSize: 18,
+        color: '#101026',
+        fontWeight: 'bold'
+    }
+});
